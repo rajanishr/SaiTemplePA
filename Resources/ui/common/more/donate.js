0c7740e873e299c2cf7063f363e191c4ee93ccbb
Rajanish Rajvanshi <rajvanshi@hotmail.com>
2013-05-11 16:08:05 -0400

    x

--- Resources/ui/common/more/donate.js
+++ Resources/ui/common/more/donate.js
@@ -3,14 +3,23 @@ function Donate() {
 		backgroundColor:'#fff'
 	});
 	
-	var label = Ti.UI.createLabel({
-		color:'#000000',
-		text:'Donate',
-		height:'auto',
-		width:'auto'
+
+	Ti.App.fireEvent('show_indicator');
+	Titanium.Platform.openURL('https://Simplecheckout.authorize.net/payment/CatalogPayment.aspx');
+	/*
+	var webview = Ti.UI.createWebView();
+	self.add(webview);
+	 
+	var xhr = Ti.Network.createHTTPClient();
+	xhr.open('POST', 'https://Simplecheckout.authorize.net/payment/CatalogPayment.aspx');
+	xhr.onload = function () {
+	    webview.html = this.responseText;
+	};
+	xhr.send({
+	    LinkId: 'ab9feda8-7e88-42e5-9696-730863e9fa0e'
 	});
-	self.add(label);
-	
+	*/
+ 
 	return self;
 };
 
